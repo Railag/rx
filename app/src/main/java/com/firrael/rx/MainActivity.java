0e85af152660f46772e6b400ca260d7204f3c7e8
Railag <whenthegroundcavedin@gmail.com>
2016-05-24 13:15:43 +0300

    nucleus and icepick setup, base presenter for MVP

--- app/src/main/java/com/firrael/rx/MainActivity.java
+++ app/src/main/java/com/firrael/rx/MainActivity.java
@@ -7,7 +7,6 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,13 +14,14 @@ import android.widget.TextView;
 
 import java.util.List;
 
+import nucleus.view.NucleusAppCompatActivity;
 import rx.Observable;
 import rx.Subscriber;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends NucleusAppCompatActivity<MainPresenter>
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
---
+++ app/src/main/java/com/firrael/rx/MainPresenter.java
@@ -0,0 +1,8 @@
+package com.firrael.rx;
+
+/**
+ * Created by firrael on 24.05.2016.
+ */
+public class MainPresenter extends BasePresenter<MainActivity> {
+
+}
